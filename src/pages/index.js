2359b7962d1e3dc5ead94ec0cdd3b30074396419
ogata toshiki <ogata.toshiki0805@gmail.com>
2019-07-02 22:52:27 +0900

    update: 注目記事の表示

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,10 @@ class BlogIndex extends React.Component {
     const { data, location } = this.props
     const { title, totalPosts } = data.site.siteMetadata
     const posts = data.allMarkdownRemark.edges
+    const pickUpPosts = ['/test1/', '/test2/', '/test3/']
+    const pickUpFilterPosts = posts.filter(post =>
+      pickUpPosts.includes(post.node.fields.slug)
+    )
 
     return (
       <Layout location={location} title={title}>
@@ -29,18 +33,14 @@ class BlogIndex extends React.Component {
           title="All posts"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        {/* <Section>
+        <Section>
           <Heading main="PICK UP" sub="注目の記事" />
-          <ArticleWrapper>
-            {posts.map(({ node }) => {
-              if (node.fields.slug === "/test1/" || node.fields.slug === "/test2/" || node.fields.slug === "/test3/") {
-                return (
-                  <Article key={node.fields.slug} link={node.fields.slug} title={node.frontmatter.title} date={node.frontmatter.date} tmb={node.frontmatter.tmb} />
-                )
-              }
-            })}
-          </ArticleWrapper>
-        </Section> */}
+          <Article
+            posts={pickUpFilterPosts}
+            ref={this.Article}
+            totalPosts={totalPosts}
+          />
+        </Section>
         <Section>
           <Heading main="NEW POSTS" sub="新着記事" />
           <Article posts={posts} ref={this.Article} totalPosts={totalPosts} />
