d3c9b54b340836f27303ecf5fafdc3dd9167c141
ogata toshiki <ogata.toshiki0805@gmail.com>
2019-07-11 00:28:20 +0900

    add: gatsby-plugin-catch-links

--- gatsby-config.js
+++ gatsby-config.js
@@ -144,6 +144,7 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-catch-links`,
     {
       resolve: `gatsby-plugin-sitemap`,
       options: {
