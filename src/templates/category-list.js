7d87f4597ec0ad56bb146ce4cdd026e91ddfc221
ogata toshiki <ogata.toshiki0805@gmail.com>
2019-07-10 18:47:48 +0900

    test

--- src/templates/category-list.js
+++ src/templates/category-list.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import styled from 'styled-components'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import SEO from '../components/SEO'
 import Heading from '../components/Heading'
 import Article from '../components/Article'
