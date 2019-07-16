cb3d515df4d1bf1250e321b1321c632abc75b84f
ogata toshiki <ogata.toshiki0805@gmail.com>
2019-07-17 00:01:50 +0900

    add: gatsby-plugin-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -145,6 +145,7 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-catch-links`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-sitemap`,
       options: {
