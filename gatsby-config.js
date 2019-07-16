119e05f185ec83978446d85602cbe34a276c7140
ogata toshiki <ogata.toshiki0805@gmail.com>
2019-07-16 15:05:38 +0900

    update: site config

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
         setup(ref) {
           const ret = ref.query.site.siteMetadata.rssMetadata
           ret.allMarkdownRemark = ref.query.allMarkdownRemark
-          ret.generator = 'GatsbyJS GCN Starter'
+          ret.generator = 'GatsbyJS GSB Starter'
           return ret
         },
         query: `
